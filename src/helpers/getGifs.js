0b435dc17ba084edbfe6123d473c34aa4ee8de10
Marvin Garmendia <mgarmendiaz@smartperfectsystems.com>
2024-01-15 12:19:20 -0600

    getGifs.js Updated with https

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 
 export const getGifs = async( category ) => {
 
-    const url = `http://api.giphy.com/v1/gifs/search?api_key=dloULfTeGAjcZez5ry0M0eRp98YmiQVJ&q=${ category }&limit=10`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=dloULfTeGAjcZez5ry0M0eRp98YmiQVJ&q=${ category }&limit=10`;
     const resp = await fetch( url );
     const { data } = await resp.json();
     
